ead79c8557123dc440053e6734e861ed9a47fac9
Utkarsh Tyagi <128183831+tyagih038@users.noreply.github.com>
2026-03-04 03:36:58 +0000

    Time: 662 ms (85.25%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,6 +1,8 @@
-select product_id, year as first_year, quantity,price
-from sales
-where (product_id,year)in(select product_id, min(year) 
-from sales
-group by product_id)
-;
+select s.product_id, s.year as first_year, s.quantity, s.price 
+from Sales as s
+join (select product_id, min(year) as first_year
+from sales 
+group by product_id
+) as first_sales
+on s.product_id = first_sales.product_id
+and s.year = first_sales.first_year;
